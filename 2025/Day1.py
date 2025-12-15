87c8a787f43baf1973375cfc64008837de2d9c85
rgiro <ricardosantos@infodinamica.pt>
2025-12-15 09:49:09 +0000

    Refactor Day1.py

--- 2025/Day1.py
+++ 2025/Day1.py
@@ -1,56 +1,51 @@
 from pathlib import Path
 import math
 
-data: list[str] = (lambda: Path(__file__).with_name('Day1Part2Example.txt').read_text(encoding="utf-8").splitlines())()
-spinCounter: int = 0
-dial: int = 50
-zeroes: int = 0
+data: list[str] = (lambda: Path(__file__).with_name('Day1Part2TestData.txt').read_text(encoding="utf-8").splitlines())()
 
 # Part 1
+dial: int = 100000050
+zeroes: int = 0
+
 for line in data:
     initDial = dial
     move = int(line[1:])
+    mult = 1 if line[0] == 'R' else -1
 
-    if line[0] == 'R':
-        dial = (dial + move) % 100
-    else:
-        dial = (dial - move) % 100
+    end_pos = dial + mult * move
+    dial = end_pos % 100
 
-    if dial == 0: zeroes += 1
-    spinCounter += 1
+    if dial == 0:
+        zeroes += 1
 
-    print(f'{initDial} - {line} - {dial}')
-    spinCounter += 1
+    print(f'Rotating {line[0]} by {move}, dial now at {dial}')
 
 print('Part 1 zeros: ' + str(zeroes))
 
 print('')
 print('--- PART 2 ---')
-# Part 2
-# Situations where dial will touch 0
-# 1 - Ends on 0
-# 2 - Crosses 0 going right or left
-#   If we think of 0 as a multiple of 100, we can move from dialPos to dialPos +/- move and check for multiples of 100 in a straight line
+
+# Part 2: count times the dial hits 0 while moving along the straight line
 dial: int = 50
 zeroes: int = 0
 
 for line in data:
     initDial = dial
     move = int(line[1:])
+    mult = 1 if line[0] == 'R' else -1
 
-    if line[0] == 'R':
-        end_pos: int = dial + move
-        multiples_of_100: int = math.floor(end_pos / 100) - math.floor(dial / 100)
-        
-        dial = (end_pos) % 100
-        zeroes += multiples_of_100
+    end_pos = dial + mult * move
+
+    if mult == 1:
+        multiples_of_100 = math.floor(end_pos / 100) - math.floor(dial / 100)
     else:
-        end_pos: int = dial - move
-        multiples_of_100: int = math.floor(dial / 100) - math.floor(end_pos / 100)
-        
-        dial = (end_pos) % 100
-        zeroes += multiples_of_100
+        multiples_of_100 = math.floor(dial / 100) - math.floor(end_pos / 100)
 
-    print(f'{initDial} - {line} - {dial} / {zeroes}')
+    dial = end_pos % 100
+    if dial == 0 and (end_pos > 100 or end_pos < 0):
+        multiples_of_100 += 1
+    zeroes += multiples_of_100
+
+    print(f'Rotating {line[0]} by {move}, dial now at {dial}')
 
 print(zeroes)
